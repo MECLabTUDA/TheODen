8b35b5df43f47a32f0a3ce47dbd5b8407d209cb5
Nick Lemke <nicklemke@web.de>
2025-06-14 18:42:23 +0000

    version

--- theoden/__init__.py
+++ theoden/__init__.py
@@ -18,7 +18,7 @@ from . import watcher as watcher
 
 from .start import start_client, start_server, start_storage
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 
 print(
     f""" _____  _             ___   ____               
